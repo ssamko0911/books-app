f9a141e5fbbeda941b252dc436c5a443164b380d
ssamko0911 <ssamko091184@gmail.com>
2025-09-13 19:45:58 +0100

    Updated nav

--- views/main/nav.php
+++ views/main/nav.php
@@ -1,9 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
     <div class="container-fluid">
+        <!-- Brand -->
         <a class="navbar-brand" href="/">📚 BookRec</a>
-        <div class="d-flex">
-            <a href="login.php" class="btn btn-outline-light me-2">Login</a>
-            <a href="register.php" class="btn btn-warning">Sign Up</a>
+
+        <!-- Main links -->
+        <div class="collapse navbar-collapse justify-content-between">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="/books">Books</a>
+                </li>
+            </ul>
+
+            <!-- Login / Sign Up -->
+            <div class="d-flex">
+                <a href="login.php" class="btn btn-outline-light me-2">Login</a>
+                <a href="register.php" class="btn btn-warning">Sign Up</a>
+            </div>
         </div>
     </div>
 </nav>
