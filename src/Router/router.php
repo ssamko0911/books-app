5d7d1ea8d7946388b4cba992cab722a97bcc1de0
ssamko0911 <ssamko091184@gmail.com>
2025-09-13 20:54:36 +0100

    Updated router

--- src/Router/router.php
+++ src/Router/router.php
@@ -1,19 +1,35 @@
 <?php declare(strict_types=1);
 
+use App\Controller\BookController;
 use App\Utils\UrlTool;
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $routes = [
-    '/' => '/../Controller/books.php',
-    '/books' => '/../Controller/books.php',
+    '/' => [BookController::class, 'index'],
+    '/books' => [BookController::class, 'index'],
+    '/books/{id}' => [BookController::class, 'show'],
 ];
 
 function route($uri, $routes)
 {
-    if (array_key_exists($uri, $routes)) {
-        require __DIR__ . $routes[$uri];
-    } else {
+    $routeFound = false;
+    foreach ($routes as $pattern => [$controllerClass, $method]) {
+        $regex = preg_replace('#\{[a-zA-Z_]+\}#', '([0-9]+)', $pattern);
+        $regex = "#^$regex$#";
+
+        if (preg_match($regex, $uri, $matches)) {
+            array_shift($matches);
+
+            $controller = new $controllerClass();
+            $matches = array_map(static fn($v) => (int)$v, $matches);
+            $controller->$method(...$matches);
+            $routeFound = true;
+            break;
+        }
+    }
+
+    if (!$routeFound) {
         UrlTool::abort();
     }
 }
