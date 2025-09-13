a6cc45fb0c96342514f03c9db9c10f02dc571f70
ssamko0911 <ssamko091184@gmail.com>
2025-09-13 19:47:00 +0100

    Refactored controller

--- src/Controller/books.php
+++ src/Controller/books.php
@@ -2,6 +2,13 @@
 
 $heading = 'Available Books';
 
+$db = \App\Database\Database::getInstance();
+
+$statement = $db->query("SELECT * FROM `book_recommendations_books`");
+$books = $statement->fetchAll();
+
+//\App\Utils\Debug::dd($books);
+
 ob_start();
 require __DIR__ . '/../../views/books/index.view.php';
 $slot = ob_get_clean();
