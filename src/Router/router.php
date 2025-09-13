4f6f338dad17e8687da24865bfac48e3d334028c
ssamko0911 <ssamko091184@gmail.com>
2025-09-13 18:57:03 +0100

    Added router

---
+++ src/Router/router.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+use App\Utils\UrlTool;
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => '/../Controller/index.php',
+];
+
+function route($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require __DIR__ . $routes[$uri];
+    } else {
+        UrlTool::abort();
+    }
+}
+
+route($uri, $routes);
