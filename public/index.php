c72794cde3bf284e666926218d87266d0d3f8f32
ssamko0911 <ssamko091184@gmail.com>
2025-09-13 19:47:30 +0100

    Updated index

--- public/index.php
+++ public/index.php
@@ -1,21 +1,6 @@
 <?php declare(strict_types=1);
 
-$host = $_ENV['DB_HOST'];
-$db = $_ENV['MYSQL_DATABASE'];
-$user = $_ENV['MYSQL_USER'];
-$password = $_ENV['MYSQL_PASSWORD'];
-$certPath = __DIR__ . $_ENV['CERT_PATH'];
+require __DIR__ . '/../vendor/autoload.php';
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password, [
-        PDO::MYSQL_ATTR_SSL_CA => $certPath,
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ]);
-    echo 'Connected';
+require __DIR__ . '/../src/Router/router.php';
 
-    return $pdo;
-} catch (PDOException $e) {
-    echo 'Not connected: ' . $e->getMessage();
-}
