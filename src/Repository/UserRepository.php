63af6c053a1b0be9e39bef19ed724a2fb582e192
ssamko0911 <ssamko091184@gmail.com>
2025-09-13 22:51:05 +0100

    Added UserRepository

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Database\Database;
+use PDO;
+
+final readonly class UserRepository
+{
+    private PDO $connection;
+
+    public function __construct()
+    {
+        $this->connection = Database::getInstance();
+    }
+
+    public function createUser(string $name, string $email, string $password): bool|string
+    {
+        $stmt = $this->connection->prepare("INSERT INTO `book_recommendations_users` (username,email,password) VALUES (?,?,?)");
+        $stmt->execute([
+            $name,
+            $email,
+            password_hash($password, PASSWORD_BCRYPT),
+        ]);
+
+        return $this->connection->lastInsertId();
+    }
+
+    public function findByEmail(string $email): array|false
+    {
+        $stmt = $this->connection->prepare("SELECT * FROM `book_recommendations_users` WHERE email = ?");
+        $stmt->execute([$email]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
