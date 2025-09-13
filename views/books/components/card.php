3462d2057c52e8f43783732a666300dd951a09cf
ssamko0911 <ssamko091184@gmail.com>
2025-09-13 20:54:00 +0100

    Updated views

--- views/books/components/card.php
+++ views/books/components/card.php
@@ -1,13 +1,13 @@
 <div class="col-md-4">
     <div class="card h-100 shadow-sm">
-        <img src="<?= htmlspecialchars($book['cover_url']) ?>" class="card-img-top" alt="cover">
+<!--        <img src="--><?php //= htmlspecialchars($book['cover_url']) ?><!--" class="card-img-top" alt="cover">-->
         <div class="card-body">
             <h5 class="card-title"><?= htmlspecialchars($book['title']) ?></h5>
             <p class="card-text">
-                <strong>Author:</strong> <?= htmlspecialchars($book['author']) ?><br>
-                <span class="badge bg-primary"><?= htmlspecialchars($book['genre']) ?></span>
+                <strong>Author:</strong> <?= htmlspecialchars($book['author_id']) ?><br>
+                <span class="badge bg-primary"><?= htmlspecialchars($book['created_at']) ?></span>
             </p>
-            <a href="book.php?id=<?= urlencode($book['id']) ?>" class="btn btn-outline-primary">
+            <a href="books/<?= urlencode($book['id']) ?>" class="btn btn-outline-primary">
                 View Recommendations
             </a>
         </div>
