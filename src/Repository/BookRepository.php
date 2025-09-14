b713b28b2a4471290825edcd7d7cc58a31631c28
ssamko0911 <ssamko091184@gmail.com>
2025-09-14 17:28:08 +0100

    Updated BookRepository

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -28,16 +28,32 @@ final class BookRepository
         return $statement->fetch(PDO::FETCH_ASSOC) ?: null;
     }
 
-    public function create(string $title, string $author, ?string $description = null, ?int $publishedYear = null): bool|string
+    public function create(string $title, string $author, int $user, ?string $description = null, ?int $publishedYear = null ): bool|string
     {
-        $stmt = $this->connection->prepare("INSERT INTO `book_recommendations_books` (title,author_id,description,published_year) VALUES (?,?,?,?)");
+        $stmt = $this->connection->prepare("INSERT INTO `book_recommendations_books` (title,author_id,description,published_year,added_by_user) VALUES (?,?,?,?,?)");
         $stmt->execute([
             $title,
             $author,
             $description,
-            $publishedYear
+            $publishedYear,
+            $user,
         ]);
 
         return $this->connection->lastInsertId();
+    }
+
+    public function update(int $id, array $data): bool
+    {
+        $stmt = $this->connection->prepare(
+            "UPDATE `book_recommendations_books` SET title = :title, author_id = :author_id, description = :description, published_year = :published_year, added_by_user = :added_by_user"
+        );
+
+        return $stmt->execute([
+            ':title' => $data["title"],
+            ':author_id' => $data["author_id"],
+            ':description' => $data["description"],
+            ':published_year' => $data["published_year"],
+            ':added_by_user' => $data["added_by_user"],
+        ]);
     }
 }
