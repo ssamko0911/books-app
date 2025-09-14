7886acd7ff1278d2a20fcc867615076382885e48
ssamko0911 <ssamko091184@gmail.com>
2025-09-14 16:34:09 +0100

    Updated AppStrings

--- src/Enum/AppStrings.php
+++ src/Enum/AppStrings.php
@@ -6,6 +6,7 @@ enum AppStrings: string
 {
     case WRONG_INPUT = 'Wrong email or password';
     case EMAIL_EXISTS = 'Email already in use';
-
     case REG_FAILED = 'Registration failed. Please try again.';
+    case INTERNAL_SERVER_ERROR = 'Internal server error. Try again later';
+    case NOT_FOUND = 'Sorry, page not found.';
 }
