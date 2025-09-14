80ace788330cffce6e69aea09b5531af0b167d00
ssamko0911 <ssamko091184@gmail.com>
2025-09-14 11:25:46 +0100

    Updated BookController

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -1,12 +1,15 @@
 <?php declare(strict_types=1);
 
 namespace App\Controller;
+
+use App\Enum\Path;
 use App\Repository\BookRepository;
 use App\Utils\UrlTool;
 
 class BookController
 {
     private BookRepository $bookRepository;
+
     public function __construct()
     {
         $this->bookRepository = new BookRepository();
@@ -32,5 +35,39 @@ class BookController
         UrlTool::view('/../../views/books/show.view.php', [
             'book' => $book,
         ]);
+    }
+
+    public function showAddBookForm(): void
+    {
+        if (!isset($_SESSION['user_id'])) {
+            UrlTool::view(Path::LOGIN->value);
+            exit();
+        }
+
+        UrlTool::view(Path::ADD_BOOK->value);
+    }
+
+    public function store(): void
+    {
+        if (!isset($_SESSION['user_id'])) {
+            UrlTool::view(Path::LOGIN->value);
+            exit();
+        }
+
+        if ('POST' === $_SERVER['REQUEST_METHOD']) {
+            $title = $_POST['title'] ?? '';
+            $author_id = $_POST['author'] ?? '';
+            $description = $_POST['description'] ?? '';
+            $year = $_POST['published_year'] ?? null;
+
+            if ($title && $author_id) {
+                $this->bookRepository->create($title, $author_id, $description, $year ? (int)$year : null);
+                UrlTool::view(Path::BOOKS_LIST->value, [
+                        'books' => $this->bookRepository->getAll(),
+                    ]
+                );
+                exit;
+            }
+        }
     }
 }
