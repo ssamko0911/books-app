d5aaa1f7fc725b3e0dfbe3dad1d8cf2e297732e3
ssamko0911 <ssamko091184@gmail.com>
2025-09-14 10:45:03 +0100

    Updated path

--- src/Enum/Path.php
+++ src/Enum/Path.php
@@ -6,5 +6,7 @@ enum Path: string
     case MAIN_TEMPLATE = '/../../views/template.view.php';
     case HTTP_RESPONSE_TEMPLATE = '/../../views/$code.php';
     case LOGIN = '/../../views/auth/components/login.php';
-    case REGISTER = '/../../views/auth/components/register.php';
+    case REGISTER = '/../../views/auth/components/registration.php';
+    case RECOMMENDATIONS_LIST = '/../../views/recommendations/index.view.php';
+    case RECOMMENDATIONS_SHOW = '/../../views/recommendations/show.view.php';
 }
