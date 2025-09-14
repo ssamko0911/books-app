9cb922e6aab5243756d4ba06845f6be145750bc3
ssamko0911 <ssamko091184@gmail.com>
2025-09-14 10:44:17 +0100

    Added RecommendationsController

--- src/Controller/RecommendationsController.php
+++ src/Controller/RecommendationsController.php
@@ -2,7 +2,39 @@
 
 namespace App\Controller;
 
+use App\Enum\Path;
+use App\Repository\RecommendationRepository;
+use App\Utils\UrlTool;
+
 class RecommendationsController
 {
+    private RecommendationRepository $recommendationRepository;
 
+    public function __construct()
+    {
+        $this->recommendationRepository = new RecommendationRepository();
+    }
+
+    public function index(): void
+    {
+        $recommendations = $this->recommendationRepository->getAll();
+
+        UrlTool::view(Path::RECOMMENDATIONS_LIST->value, [
+                'recommendations' => $recommendations,
+            ]
+        );
+    }
+
+    public function show(int $id): void
+    {
+        $recommendation = $this->recommendationRepository->findOneById($id);
+
+        if (null === $recommendation) {
+            UrlTool::abort();
+        }
+
+        UrlTool::view(Path::RECOMMENDATIONS_SHOW->value, [
+            'recommendation' => $recommendation,
+        ]);
+    }
 }
