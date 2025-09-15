2a5d97d9a4d4fa505cb19396bf476ecaee3b2a78
ssamko0911 <ssamko091184@gmail.com>
2025-09-15 22:31:49 +0100

    Updated Author

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -3,49 +3,22 @@
 namespace App\Entity;
 final class Author
 {
-    private ?int $id = null;
+    private ?int $id;
     private string $firstName;
     private string $lastName;
     private string $biography;
 
+    public function __construct(?int $id, string $firstName, string $lastName, string $biography)
+    {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->biography = $biography;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function setId(?int $id): void
-    {
-        $this->id = $id;
-    }
-
-    public function getFirstName(): string
-    {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName): void
-    {
-        $this->firstName = $firstName;
-    }
-
-    public function getLastName(): string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(string $lastName): void
-    {
-        $this->lastName = $lastName;
-    }
-
-    public function getBiography(): string
-    {
-        return $this->biography;
-    }
-
-    public function setBiography(string $biography): void
-    {
-        $this->biography = $biography;
     }
 
     public function getFullName(): string
