082898a4335d04f6d1fb3fc4575d72dae56c702f
ssamko0911 <ssamko091184@gmail.com>
2025-09-15 22:24:58 +0100

    Updated BookRepository

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -2,7 +2,24 @@
 
 namespace App\Repository;
 
+use PDO;
+
 final class BookRepository extends BaseRepository
 {
     protected string $table = 'book_recommendations_books';
+    protected string $joiningTable = 'book_recommendations_authors';
+
+    public function getAllWithAuthors(): array
+    {
+        return $this->connection->query("SELECT b.*, a.first_name, a.last_name FROM {$this->table} b JOIN {$this->joiningTable} a ON a.id = b.author_id")->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function findOneByIdWithAuthor(int $id): ?array
+    {
+        $statement = $this->connection->prepare("SELECT b.*, a.first_name, a.last_name FROM {$this->table} b JOIN {$this->joiningTable} a ON a.id = b.author_id WHERE b.id = :id");
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetch(PDO::FETCH_ASSOC) ?: null;
+    }
 }
