ae8801621692cd7096aba5be32589fa02dd5db2c
ssamko0911 <ssamko091184@gmail.com>
2025-09-16 12:31:22 +0100

    Updated BookRepository

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -2,6 +2,9 @@
 
 namespace App\Repository;
 
+use App\Dto\BookDTO;
+use App\Entity\Author;
+use App\Entity\Book;
 use PDO;
 
 final class BookRepository extends BaseRepository
@@ -14,12 +17,58 @@ final class BookRepository extends BaseRepository
         return $this->connection->query("SELECT b.*, a.first_name, a.last_name FROM {$this->table} b JOIN {$this->joiningTable} a ON a.id = b.author_id")->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function findOneByIdWithAuthor(int $id): ?array
+    public function findOneByIdWithAuthor(int $id): ?Book
     {
-        $statement = $this->connection->prepare("SELECT b.*, a.first_name, a.last_name FROM {$this->table} b JOIN {$this->joiningTable} a ON a.id = b.author_id WHERE b.id = :id");
+        $statement = $this->connection->prepare("SELECT b.*, a.first_name, a.last_name, a.bio FROM {$this->table} b JOIN {$this->joiningTable} a ON a.id = b.author_id WHERE b.id = :id");
         $statement->bindParam(':id', $id, PDO::PARAM_INT);
         $statement->execute();
 
-        return $statement->fetch(PDO::FETCH_ASSOC) ?: null;
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+        if (!$row) {
+            return null;
+        }
+
+        $author = new Author(
+            id: $row['author_id'],
+            firstName: $row['first_name'],
+            lastName: $row['last_name'],
+            biography: $row['bio'],
+        );
+
+        return new Book(
+            id: $row['id'],
+            title: $row['title'],
+            author: $author,
+            description: $row['description'],
+            publishedYear: $row['published_year'],
+            addedByUserId: $row['added_by_user'],
+        );
+    }
+
+    public function createFromDTO(BookDTO $bookDto): bool|string
+    {
+        $data = [
+            'title' => $bookDto->title,
+            'author_id' => $bookDto->author->id,
+            'description' => $bookDto->description,
+            'published_year' => $bookDto->publishedYear,
+            'added_by_user' => $bookDto->addedByUserId,
+        ];
+
+        return $this->create($data);
+    }
+
+    public function updateFromDTO(BookDTO $bookDto): bool|string
+    {
+        $data = [
+            'title' => $bookDto->title,
+            'author_id' => $bookDto->author->id,
+            'description' => $bookDto->description,
+            'published_year' => $bookDto->publishedYear,
+            'added_by_user' => $bookDto->addedByUserId,
+        ];
+
+        return $this->update($bookDto->id, $data);
     }
 }
