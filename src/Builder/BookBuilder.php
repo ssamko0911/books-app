e457b2aa0a0681e22d3904c8fe79d941ef151981
ssamko0911 <ssamko091184@gmail.com>
2025-09-16 12:31:04 +0100

    Added BookBuilder

---
+++ src/Builder/BookBuilder.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace App\Builder;
+
+use App\Dto\AuthorSelectDTO;
+use App\Dto\BookDTO;
+use App\Entity\Book;
+
+final class BookBuilder
+{
+    public function buildBookDTO(array $data): BookDTO
+    {
+        $bookDto = new BookDTO();
+
+        $id = (int)($data['book_id'] ?? null);
+        $title = $data['title'] ?? '';
+        $authorId =  (int)$data['author'];
+        $authorFullName = $data['author_name'] ?? '';
+        $description = $data['description'] ?? '';
+        $publishedYear = $data['published_year'] ?? null;
+        $addedByUserId = $_SESSION['user_id'];
+
+        $bookDto->id = $id;
+        $bookDto->title = $title;
+
+        $authorDto = new AuthorSelectDTO();
+        $authorDto->id = $authorId;
+        $authorDto->fullName = $authorFullName;
+
+        $bookDto->author = $authorDto;
+        $bookDto->description = $description;
+        $bookDto->publishedYear = $publishedYear ? (int)$publishedYear : null;
+        $bookDto->addedByUserId = $addedByUserId;
+
+        return $bookDto;
+    }
+
+    public function buildBookDTOFromEntity(Book $book): BookDTO
+    {
+        $bookDto = new BookDTO();
+        $bookDto->id = $book->getId();
+        $bookDto->title = $book->getTitle();
+
+        $authorDto = new AuthorSelectDTO();
+        $authorDto->id = $book->getAuthor()->getId();
+        $authorDto->fullName = $book->getAuthor()->getFullName();
+
+        $bookDto->author = $authorDto;
+        $bookDto->description = $book->getDescription();
+        $bookDto->publishedYear = $book->getPublishedYear();
+        $bookDto->addedByUserId = $book->getAddedByUserId();
+
+        return $bookDto;
+    }
+}
