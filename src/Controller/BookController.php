0930dc5a8bc1aa0eae07c16600868b1505831261
ssamko0911 <ssamko091184@gmail.com>
2025-09-20 19:59:20 +0100

    Updated BookController

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,43 +2,27 @@
 
 namespace App\Controller;
 
-use App\Builder\AuthorBuilder;
-use App\Builder\BookEntityBuilder;
-use App\Dto\AuthorSelectDTO;
-use App\Entity\Author;
 use App\Enum\AppStrings;
 use App\Enum\Path;
-use App\Repository\AuthorRepository;
-use App\Repository\BookRepository;
+use App\Service\BookService;
 use App\Utils\Logger;
 use App\Utils\SanitizerImpls\BookSanitizer;
 use App\Utils\ValidatorImpls\BookValidator;
 use JetBrains\PhpStorm\NoReturn;
-use PDO;
 use PH7\JustHttp\StatusCode;
 
 final class BookController extends BaseController
 {
-    private BookRepository $repository;
-    private AuthorBuilder $authorBuilder;
-    private AuthorRepository $authorRepository;
-    private BookEntityBuilder $builder;
-
-    public function __construct(PDO $connection)
+    public function __construct(
+        private readonly BookService $service,
+    )
     {
-        $this->repository = new BookRepository($connection);
-        $this->authorBuilder = new AuthorBuilder();
-        $this->builder = new BookEntityBuilder();
-        $this->authorRepository = new AuthorRepository($connection);
     }
 
     public function index(): void
     {
-        $books = $this->repository->getAllWithAuthors();
-        $bookDTOs = $this->builder->buildDTOs($books);
-
         $this->render(Path::BOOKS_LIST->value, [
-            'books' => $bookDTOs,
+            'books' => $this->service->getAllBooksWithAuthors(),
         ]);
     }
 
@@ -46,16 +30,14 @@ final class BookController extends BaseController
     {
         $this->requireLogin();
 
-        $book = $this->repository->findOneByIdWithAuthor($id);
+        $book = $this->service->getBookWithAuthor($id);
 
         if (null === $book) {
             $this->abort();
         }
 
-        $bookDto = $this->builder->buildDTO($book) ;
-
         $this->render(Path::SHOW_BOOK->value, [
-            'book' => $bookDto,
+            'book' => $book,
         ]);
     }
 
@@ -63,11 +45,7 @@ final class BookController extends BaseController
     {
         $this->requireLogin();
 
-        $authors = $this->authorRepository->getForDropdown();
-
-        $authorDTOs = array_map(function (Author $author): AuthorSelectDTO {
-            return $this->authorBuilder->buildSelectDTO($author);
-        }, $authors);
+        $authors = $this->service->getAuthorsForBookInsert();
 
         $errors = $_SESSION['errors'] ?? [];
         $old = $_SESSION['old'] ?? [];
@@ -75,60 +53,57 @@ final class BookController extends BaseController
         unset($_SESSION['errors'], $_SESSION['old']);
 
         $this->render(Path::ADD_BOOK->value, [
-            'authors' => $authorDTOs,
+            'authors' => $authors,
             'errors' => $errors,
             'old' => $old,
         ]);
     }
 
-    public function store(): void
+    #[NoReturn] public function store(): void
     {
         $this->requireLogin();
 
-        if ('POST' === $_SERVER['REQUEST_METHOD']) {
-            $sanitized = BookSanitizer::sanitize($_POST);
-            $errors = BookValidator::validate($sanitized);
-
-            if([] !== $errors) {
-                $_SESSION['errors'] = $errors;
-                $_SESSION['old'] = $sanitized;
-                $this->redirect('/books/add');
-            }
-
-            $bookDto = $this->builder->buildDTOFromRequest($sanitized, $_SESSION['user_id']);
-            $this->repository->createFromDTO($bookDto);
-            $this->redirect('/books');
+        if ('POST' !== $_SERVER['REQUEST_METHOD']) {
+            $this->redirect('/books/add');
         }
+
+        $sanitized = BookSanitizer::sanitize($_POST);
+        $errors = BookValidator::validate($sanitized);
+
+        if ([] !== $errors) {
+            $_SESSION['errors'] = $errors;
+            $_SESSION['old'] = $sanitized;
+            $this->redirect('/books/add');
+        }
+
+        $this->service->createBook($sanitized);
+        $this->redirect('/books');
     }
 
     public function showEditBookForm(int $id): void
     {
         $this->requireLogin();
 
-        $book = $this->repository->findOneByIdWithAuthor($id);
+        $book = $this->service->getBookWithAuthor($id);
 
         if (null === $book) {
             $this->abort();
         }
 
-        if ('admin' !== $_SESSION['role'] && $book->getAddedByUserId() !== $_SESSION['user_id']) {
+        if ('admin' !== $_SESSION['role'] && $book->addedByUserId !== $_SESSION['user_id']) {
             Logger::getLogger()->warning(AppStrings::NOT_AUTHORISED_EDIT->value, [
-                'book_id' => $book['id'],
+                'book_id' => $book->id,
                 'user_id' => $_SESSION['user_id'] ?? null,
             ]);
 
             $this->abort(StatusCode::FORBIDDEN);
         }
 
-        $authors = $this->authorRepository->getForDropdown();
-
-        $authorDTOs = array_map(function (Author $author): AuthorSelectDTO {
-            return $this->authorBuilder->buildSelectDTO($author);
-        }, $authors);
+        $authors = $this->service->getAuthorsForBookInsert();
 
         $this->render(Path::EDIT_BOOK->value, [
-            'book' => $this->builder->buildDTO($book),
-            'authors' => $authorDTOs,
+            'book' => $book,
+            'authors' => $authors,
         ]);
     }
 
@@ -136,14 +111,13 @@ final class BookController extends BaseController
     {
         $this->requireLogin();
 
-        $book = $this->repository->findOneByIdWithAuthor($id);
+        $book = $this->service->getBookWithAuthor($id);
 
         if (null === $book) {
             $this->abort();
         }
 
-        $bookDto = $this->builder->buildDTOFromRequest($_POST, $_SESSION['user_id']);
-        $this->repository->updateFromDTO($bookDto);
+        $this->service->updateBook($_POST);
 
         $this->redirect('/books/' . $id);
     }
@@ -162,13 +136,9 @@ final class BookController extends BaseController
             $this->abort(StatusCode::FORBIDDEN);
         }
 
-        $book = $this->repository->findOneById($id);
-
-        if (null === $book) {
+        if (!$this->service->deleteBook($id)) {
             $this->abort();
         }
-
-        $this->repository->delete($id);
 
         Logger::getLogger()->info(AppStrings::BOOK_DELETE->value, [
             'book_id' => $id,
