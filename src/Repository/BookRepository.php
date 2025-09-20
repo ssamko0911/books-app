2ed73b22feb298dd7bd12ad44765ac182bd130b0
ssamko0911 <ssamko091184@gmail.com>
2025-09-20 19:59:57 +0100

    Updated BookRepository

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repository;
 
 use App\Dto\BookDTO;
-use App\Entity\Author;
 use App\Entity\Book;
 use PDO;
 
@@ -17,18 +16,10 @@ final class BookRepository extends BaseRepository
      */
     public function getAllWithAuthors(): array
     {
-        $rows = $this->connection->query("SELECT b.*, a.first_name, a.last_name, a.bio FROM {$this->table} b JOIN {$this->joiningTable} a ON a.id = b.author_id")->fetchAll(PDO::FETCH_ASSOC);
-        $books = [];
-
-        foreach ($rows as $row) {
-            $book = $this->mapRowToBook($row);
-            $books[] = $book;
-        }
-
-        return $books;
+        return $this->connection->query("SELECT b.*, a.first_name, a.last_name, a.bio FROM {$this->table} b JOIN {$this->joiningTable} a ON a.id = b.author_id")->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function findOneByIdWithAuthor(int $id): ?Book
+    public function findOneByIdWithAuthor(int $id): ?array
     {
         $statement = $this->connection->prepare("SELECT b.*, a.first_name, a.last_name, a.bio FROM {$this->table} b JOIN {$this->joiningTable} a ON a.id = b.author_id WHERE b.id = :id");
         $statement->bindParam(':id', $id, PDO::PARAM_INT);
@@ -40,51 +31,32 @@ final class BookRepository extends BaseRepository
             return null;
         }
 
-        return $this->mapRowToBook($row);
+        return $row;
     }
 
     public function createFromDTO(BookDTO $bookDto): bool|string
     {
-        $data = [
-            'title' => $bookDto->title,
-            'author_id' => $bookDto->author->id,
-            'description' => $bookDto->description,
-            'published_year' => $bookDto->publishedYear,
-            'added_by_user' => $bookDto->addedByUserId,
-        ];
-
-        return $this->create($data);
+        return $this->create(
+            $this->getDTOData($bookDto)
+        );
     }
 
     public function updateFromDTO(BookDTO $bookDto): bool|string
     {
-        $data = [
-            'title' => $bookDto->title,
-            'author_id' => $bookDto->author->id,
-            'description' => $bookDto->description,
-            'published_year' => $bookDto->publishedYear,
-            'added_by_user' => $bookDto->addedByUserId,
-        ];
-
-        return $this->update($bookDto->id, $data);
+        return $this->update(
+            $bookDto->id,
+            $this->getDTOData($bookDto)
+        );
     }
 
-    public function mapRowToBook(array $row): Book
+    private function getDTOData($dto): array
     {
-        $author = new Author(
-            id: $row['author_id'],
-            firstName: $row['first_name'],
-            lastName: $row['last_name'],
-            biography: $row['bio'],
-        );
-
-        return new Book(
-            id: $row['id'],
-            title: $row['title'],
-            author: $author,
-            description: $row['description'],
-            publishedYear: $row['published_year'],
-            addedByUserId: $row['added_by_user'],
-        );
+        return [
+            'title' => $dto->title,
+            'author_id' => $dto->author->id,
+            'description' => $dto->description,
+            'published_year' => $dto->publishedYear,
+            'added_by_user' => $dto->addedByUserId,
+        ];
     }
 }
