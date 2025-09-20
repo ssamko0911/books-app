e3ee3605d6d300094dd0d8de1dfbd39186558ad5
ssamko0911 <ssamko091184@gmail.com>
2025-09-20 19:57:23 +0100

    Updated index

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php declare(strict_types=1);
 
+use App\Utils\UrlTool;
+use PH7\JustHttp\StatusCode;
+
 session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/../src/Router/routes.php';
+try {
+    require __DIR__ . '/../src/Router/routes.php';
+} catch (RuntimeException $e) {
+    // Todo: Add logging
+    UrlTool::abort(StatusCode::INTERNAL_SERVER_ERROR);
+}
 
