77f3f315b3cd354348a87ca72a51476c94467d65
ssamko0911 <ssamko091184@gmail.com>
2025-09-20 20:43:40 +0100

    Updated BookEntityBuilder

--- src/Builder/BookEntityBuilder.php
+++ src/Builder/BookEntityBuilder.php
@@ -5,31 +5,31 @@ namespace App\Builder;
 use App\Dto\BookDTO;
 use App\Entity\Book;
 
-final class BookEntityBuilder
+final readonly class BookEntityBuilder
 {
     public function __construct(
         private AuthorEntityBuilder $authorBuilder
     )
     {
     }
-    public function buildFromRow(array $row): Book
+    public function buildEntityFromRow(array $row): Book
     {
         return new Book(
             id: $row['id'],
             title: $row['title'],
-            author: $this->authorBuilder->buildFromRow($row),
+            author: $this->authorBuilder->buildEntityFromRow($row),
             description: $row['description'],
             publishedYear: $row['published_year'],
             addedByUserId: $row['added_by_user'],
         );
     }
 
-    public function buildBooksFromRow(array $rows): array
+    public function buildEntitiesFromRows(array $rows): array
     {
         $books = [];
 
         foreach ($rows as $row) {
-            $books[] = $this->buildFromRow($row);
+            $books[] = $this->buildEntityFromRow($row);
         }
 
         return $books;
@@ -55,7 +55,7 @@ final class BookEntityBuilder
         $bookDto = new BookDTO();
         $bookDto->id = $book->getId();
         $bookDto->title = $book->getTitle();
-        $bookDto->author = $this->authorBuilder->buildAuthorSelectDTO($book->getAuthor());
+        $bookDto->author = $this->authorBuilder->buildSelectDTO($book->getAuthor());
         $bookDto->description = $book->getDescription();
         $bookDto->publishedYear = $book->getPublishedYear();
         $bookDto->addedByUserId = $book->getAddedByUserId();
